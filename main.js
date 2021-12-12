f4da349f0914d7ea91381849d456b4faaed16c71
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-12-12 23:54:57 +0200

    added updates according comments

--- main.js
+++ main.js
@@ -66,33 +66,38 @@ function changeHP(player) {
     '.player' + player.player + ' .life'
   );
 
-  let normalizingZeroValue =
-    (player.hp -= Math.floor(Math.random() * (20 - 0 + 1) + 0)) < 0
-      ? (player.hp = 0)
-      : player.hp;
+  const normalizingZeroValue = pickRandomValue(player);
 
   playerLife.style.width = normalizingZeroValue + '%';
 
   if (normalizingZeroValue <= 0) {
-    randomButton.disabled = true;
-    player1.hp > player2.hp
-      ? mainArena.appendChild(playerWin(player1.name))
-      : mainArena.appendChild(playerWin(player2.name));
+    determiningWhoIsWin();
   }
-}
-
-function playerLose(name) {
-  const loseTitle = createElement('div', 'loseTitle');
-  loseTitle.innerText = name + ' lose';
-
-  return loseTitle;
 }
 
 function playerWin(name) {
   const winTitle = createElement('div', 'winTitle');
   winTitle.innerText = name + ' win';
-
   return winTitle;
+}
+
+function pickRandomValue(player) {
+  const hpChange =
+    (player.hp -= Math.floor(Math.random() * (20 - 0 + 1) + 0)) < 0
+      ? (player.hp = 0)
+      : player.hp;
+
+  return hpChange;
+}
+
+function determiningWhoIsWin() {
+  randomButton.disabled = true;
+
+  if (player1.hp > player2.hp) {
+    mainArena.appendChild(playerWin(player1.name));
+  } else {
+    mainArena.appendChild(playerWin(player2.name));
+  }
 }
 
 randomButton.addEventListener('click', () => {
